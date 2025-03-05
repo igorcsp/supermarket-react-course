298cb1570678d3a769962642c3fa7a2a97dc9417
igorcsp <icordeiro787@gmail.com>
2025-03-05 09:39:01 -0300

    completed. Optional todos: refactor with context or redux

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,8 +1,42 @@
+import Input from "../components/Input";
+import Button from "../components/Button";
+import { useState } from "react";
+import { loadStripe } from "@stripe/stripe-js";
+
+const stripeLoadedPromise = loadStripe(
+  "pk_test_51HsqkCGuhXEITAut89vmc4jtjYd7XPs8hWfo2XPef15MFqI8rCFc8NqQU9WutlUBsd8kmNqHBeEmSrdMMpeEEyfT00KzeVdate"
+);
+
 export default function Cart({ cart }) {
+  const [email, setEmail] = useState("");
+
+  const lineItemsCart = cart.map((cartItem) => {
+    return { price: cartItem.price_id, quantity: cartItem.quantity };
+  });
+
   const totalPrice = cart.reduce(
     (total, product) => total + product.price * product.quantity,
     0
   );
+
+  const handleChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email);
+
+    stripeLoadedPromise.then((stripe) => {
+      stripe.redirectToCheckout({
+        lineItems: lineItemsCart,
+        mode: "payment",
+        successUrl: "https://react-tutorial.app/app.html",
+        cancelUrl: "https://react-tutorial.app/app.html",
+        customerEmail: email,
+      });
+    });
+  };
 
   return (
     <div className="cart-layout">
@@ -54,6 +88,22 @@ export default function Cart({ cart }) {
                 </tr>
               </tfoot>
             </table>
+            <form onSubmit={handleSubmit} className="pay-form">
+              <p>
+                Enter your email and then click on pay and your products will be
+                delivered to you on the same day!
+              </p>
+              <Input
+                value={email}
+                onChange={handleChange}
+                email={email}
+                autoComplete="email"
+                placeholder="Email"
+                type="email"
+                required
+              />
+              <Button type="submit">Pay</Button>
+            </form>
           </>
         )}
       </div>
